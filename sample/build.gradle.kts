6f5e3d516527e619213a05462b33f0e6f2fc9e09
nari4 <nari4169@gmail.com>
2023-11-09 21:30:59 +0900

    half picker append 1 ~ 12, 0 ~ 59 not used AM PM

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -28,9 +28,9 @@ android {
 dependencies {
     implementation(project(":lib"))
 
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.ui:ui-tooling:${Versions.compose}")
 
     testImplementation("junit:junit:4.13.2")
